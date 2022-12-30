d65b6c0606fe538df8bf98eaa4a2b32f166ea62e
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2022-12-30 16:11:30 +0530

    Update Assignment7.cpp

--- CG/Assignment7.cpp
+++ CG/Assignment7.cpp
@@ -1,99 +1,81 @@
-//#define GLUT_DISABLE_ATEXIT_HACK
-//#include <iostream>
-////#include <windows.h>
-////#include <GL/glut.h>
-//#include <OpenGL/gl.h>
-//#include <OpenGl/glu.h>
-//#include <GLUT/glut.h>
-//#include <cmath>
-//using namespace std;
-//float y_inc = 0.0f ;
-//float color_inc = 0.0f ;
-//bool flag_position = 0 ; // UP
-//bool flag_color = 0 ;
-//
-//void init() {
-//   glClearColor( 0.0 , 0.0 , 0.0 , 1.0 ) ;
-//   glMatrixMode( GL_PROJECTION ) ;
-//   glLoadIdentity() ;
-//   gluOrtho2D( 0.0 , 500.0 , 0.0 , 500.0 ) ;
-//}
-//
-//void draw_circle(float x, float y, float radius) {
-//   glMatrixMode(GL_MODELVIEW);
-//   glPushMatrix();
-//   glLoadIdentity();
-//   glTranslatef(x, y, 0.0f);
-//   int circle_points = 100;
-//   float angle = 2.0f * 3.1416f / circle_points;
-//   glBegin(GL_POLYGON);
-//   double angle1 = 0.0 ;
-//   glVertex2d(radius * cos(0.0) , radius * sin(0.0));
-//   for ( int i=0; i<circle_points; i++) {
-//       glVertex2d(radius * cos(angle1), radius *sin(angle1));
-//       angle1 += angle;
-//   }
-//   glEnd();
-//   glPopMatrix();
-//}
-//
-//
-//void display() {
-//   glClear( GL_COLOR_BUFFER_BIT ) ;
-//
-//   glColor3f( 1.0f - (color_inc/200) , 0.64f + (color_inc/500) , 0.0f + (color_inc/102) ) ;
-//   glBegin( GL_QUADS ) ;
-//   glVertex2i( 0 , 0 ) ;
-//   glVertex2i( 0 , 500 ) ;
-//   glVertex2i( 500 , 500 ) ;
-//   glVertex2i( 500 , 0 ) ;
-//   glEnd() ;
-//
-//
-//   glColor3f( 1.0 , 1.0 , 0.0 ) ;
-//   draw_circle( y_inc * 2.5f  , 250.0f + 200.0f * sin( y_inc / 25.0f ) , 30.0f ) ;
-//
-//
-//   glFlush() ;
-//}
-//
-//void idle() {
-//   glutPostRedisplay() ;
-//
-//   if( flag_position == 0 ) {
-//       y_inc += 0.020f ;
-//   }
-//   else {
-//       y_inc -= 0.020f ;
-//   }
-//
-//   if( y_inc >= 250.0f || y_inc <= 0.0f ) {
-//       flag_position = !flag_position ;
-//   }
-//
-//   if( flag_color == 0 ) {
-//       color_inc += 0.008 ;
-//   }
-//   else {
-//       color_inc -= 0.008 ;
-//   }
-//
-//   if( color_inc >= 100.0f || color_inc <= 0.0f ) {
-//       flag_color = !flag_color ;
-//   }
-//
-//}
-//
-//
-//int main( int argc, char **argv ) {
-//   glutInit( &argc , argv ) ;
-//   // Initialize Window size and its position
-//   glutInitWindowSize( 500 , 500 );
-//   glutInitWindowPosition( 0 , 0 ) ;
-//   glutCreateWindow( "Sunrise and Sunset" );
-//   glutDisplayFunc( display ) ;
-//   glutIdleFunc( idle ) ;
-//   init() ;
-//   glutMainLoop() ;
-//   return 0;
-//}
+#include <iostream>
+#include <GLUT/GLUT.h>
+
+int boxHeight = 40;
+int boxWidth = 100;
+int bottom = -200;
+
+class Stack {
+public:
+    char arr[10];
+    int top;
+    
+    Stack() {
+        this->top = 0;
+    }
+    
+    void pop() {
+        if (top) {
+            top--;
+        }
+    }
+    
+    void push(char a) {
+        if (top < 9) {
+            arr[top++] = a;
+        }
+    }
+} stack;
+
+void renderScene();
+
+void keyboardFunc(unsigned char c, int x, int y) {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+        stack.push(c);
+    } else if (c == 127) {
+        stack.pop();
+    }
+    renderScene();
+}
+
+inline void drawBox(int x, int y) {
+    glBegin(GL_LINE_LOOP);
+    
+    glVertex3f(x, y, 0);
+    glVertex3f(x + boxWidth, y, 0);
+    glVertex3f(x + boxWidth, y + boxHeight, 0);
+    glVertex3f(x, y + boxHeight, 0);
+    
+    glEnd();
+}
+
+inline void drawStack() {
+    for (int i = 0; i < stack.top; i++) {
+        drawBox(-boxWidth / 2, bottom + i * boxHeight);
+        glRasterPos3f(-9, bottom + i * boxHeight + 12, 0);
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, stack.arr[i]);
+    }
+}
+
+void renderScene() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawStack();
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv) {
+    stack.push('A');
+    stack.push('A');
+    stack.push('A');
+    stack.push('A');
+    glutInit(&argc, argv);
+    
+    glutInitWindowSize(800, 600); // Optional
+    glutCreateWindow("Stack");
+    gluOrtho2D(-400, 400, -300, 300);
+    glutKeyboardFunc(keyboardFunc);
+    
+    glutDisplayFunc(renderScene);
+    
+    glutMainLoop();
+}
